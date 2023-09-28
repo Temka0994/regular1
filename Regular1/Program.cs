c4a1d79881b4f33128c76f4d1dd3785890f04307
Temka0994 <145697368+Temka0994@users.noreply.github.com>
2023-09-29 01:35:31 +0300

    Add files via upload

---
+++ Regular1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введiть логiн: ");
+        string login = Console.ReadLine();
+
+        if (Valid(login))
+        {
+            Console.WriteLine("Логiн пiдходить.");
+        }
+        else
+        {
+            Console.WriteLine("Логiн не пiдходить.");
+        }
+    }
+
+    static bool Valid(string login)
+    {
+        Regex myReg = new Regex(@"^[a-zA-Z][a-zA-Z0-9]{1,9}$");
+
+        if (myReg.IsMatch(login))
+        {
+            return true;
+        }
+
+        return false;   
+    }
+}
